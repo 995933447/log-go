deef924b987a76ca8ea92313e8b66066b325821d
995933447 <34621597+995933447@users.noreply.github.com>
2025-06-26 12:50:55 +0400

    Update trace.go

--- v2/loggo/logger/fmts/trace.go
+++ v2/loggo/logger/fmts/trace.go
@@ -3,7 +3,7 @@ package fmts
 import (
 	"errors"
 	"fmt"
-	logger2 "github.com/995933447/log-go/v2/loggo/logger"
+	"github.com/995933447/log-go/v2/loggo/logger"
 	"github.com/995933447/runtimeutil"
 	"path"
 	"runtime"
@@ -12,9 +12,9 @@ import (
 	"unicode/utf8"
 )
 
-var _ logger2.Formatter = (*TraceFormatter)(nil)
+var _ logger.Formatter = (*TraceFormatter)(nil)
 
-func NewTraceFormatter(moduleName string, skipCall int, formatType Format, disabledStdoutColor bool, cfgLoader *logger2.ConfLoader) *TraceFormatter {
+func NewTraceFormatter(moduleName string, skipCall int, formatType Format, disabledStdoutColor bool, cfgLoader *logger.ConfLoader) *TraceFormatter {
 	return &TraceFormatter{
 		skipCall:            skipCall,
 		formatType:          formatType,
@@ -28,7 +28,7 @@ type TraceFormatter struct {
 	skipCall            int
 	formatType          Format
 	moduleName          string
-	cfgLoader           *logger2.ConfLoader
+	cfgLoader           *logger.ConfLoader
 	disabledStdoutColor bool
 }
 
@@ -40,24 +40,24 @@ func (f *TraceFormatter) SetSkipCall(skipCall int) {
 	f.skipCall = skipCall
 }
 
-func (f *TraceFormatter) Copy() logger2.Formatter {
+func (f *TraceFormatter) Copy() logger.Formatter {
 	return NewTraceFormatter(f.moduleName, f.skipCall, f.formatType, f.disabledStdoutColor, f.cfgLoader)
 }
 
-func (f *TraceFormatter) Sprintf(level logger2.Level, stdoutColor logger2.Color, format string, args ...interface{}) (string, error) {
-	levelStr, err := logger2.TransferLevelToStr(level)
+func (f *TraceFormatter) Sprintf(level logger.Level, stdoutColor logger.Color, format string, args ...interface{}) (string, error) {
+	levelStr, err := logger.TransferLevelToStr(level)
 	if err != nil {
 		return "", err
 	}
 
 	var colorStdoutStart, colorStdoutEnd string
-	if !f.disabledStdoutColor && stdoutColor != logger2.ColorNil {
-		colorStdoutStart, err = logger2.GetColorStdout(stdoutColor)
+	if !f.disabledStdoutColor && stdoutColor != logger.ColorNil {
+		colorStdoutStart, err = logger.GetColorStdout(stdoutColor)
 		if err != nil {
 			return "", err
 		}
 
-		colorStdoutEnd = logger2.ColorToStdoutMap[logger2.ColorNil]
+		colorStdoutEnd = logger.ColorToStdoutMap[logger.ColorNil]
 	}
 
 	pc, callFile, callLine, ok := runtime.Caller(f.skipCall)
@@ -75,12 +75,12 @@ func (f *TraceFormatter) Sprintf(level logger2.Level, stdoutColor logger2.Color,
 	trace := runtimeutil.GetTrace()
 
 	switch level {
-	case logger2.LevelDebug:
+	case logger.LevelDebug:
 		debugMsgMaxLen := f.cfgLoader.GetConf().File.DebugMsgMaxLen
 		if debugMsgMaxLen > 0 && int32(utf8.RuneCountInString(rawFormatted)) > debugMsgMaxLen {
 			rawFormatted = string([]rune(rawFormatted)[:debugMsgMaxLen]) + "..."
 		}
-	case logger2.LevelInfo:
+	case logger.LevelInfo:
 		infoMsgMaxLen := f.cfgLoader.GetConf().File.InfoMsgMaxLen
 		if infoMsgMaxLen > 0 && int32(utf8.RuneCountInString(rawFormatted)) > infoMsgMaxLen {
 			rawFormatted = string([]rune(rawFormatted)[:infoMsgMaxLen]) + "..."
@@ -89,20 +89,6 @@ func (f *TraceFormatter) Sprintf(level logger2.Level, stdoutColor logger2.Color,
 
 	switch f.formatType {
 	case FormatText:
-		//return fmt.Sprintf(
-		//	"[%s.%04d] [%s] [%s][%d:%d] %s %s:%s:%d %s\n",
-		//	now.Format("2006-01-02 15:04:05"),
-		//	now.Nanosecond()/100000,
-		//	f.moduleName,
-		//	trace.TraceId,
-		//	trace.AppId,
-		//	trace.Uid,
-		//	levelStr,
-		//	callFuncName,
-		//	fileName,
-		//	callLine,
-		//	rawFormatted,
-		//), nil
 		return fmt.Sprintf(
 			"[%s.%04d] [%s] [%s] %s%s %s:%s:%d %s%s \n",
 			now.Format("2006-01-02 15:04:05"),
